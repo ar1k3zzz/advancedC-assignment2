c731a382a7e2ef6525ab8c748c1aed991b220ee8
asd3asdasfgasd asdgasdfasdf <nissocialnetwork@gmail.com>
2021-09-24 15:41:16 +0600

    initial commit

---
+++ cmake-build-debug/CMakeFiles/advancedC_assignment2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/advancedC_assignment2.dir/main.cpp.o"
+  "advancedC_assignment2.exe"
+  "advancedC_assignment2.pdb"
+  "libadvancedC_assignment2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/advancedC_assignment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
