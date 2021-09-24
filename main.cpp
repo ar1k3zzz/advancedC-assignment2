ef12fdf57ac345971c84994ce0ec8a133778c854
asd3asdasfgasd asdgasdfasdf <nissocialnetwork@gmail.com>
2021-09-24 18:06:41 +0600

    Complete Task

--- main.cpp
+++ main.cpp
@@ -36,6 +36,14 @@ void findLuckiest(vector<Person> &people){
     cout << "Luckiest Person: " << people[index].getName() << endl << "===================" << endl;
 }
 
+void changeLuck(vector<Person> &people,int index1, int index2){
+    int luckTemp1 = people[index1-1].getLuck(), luckTemp2 = people[index2-1].getLuck();
+    people[index1-1].setLuck(luckTemp2);
+    people[index2-1].setLuck(luckTemp1);
+    cout << "You have changed luck of: " << people[index1-1].getName() << " and " << people[index2-1].getName() << endl;
+    cout << "Now check the list whether changes have been made" << endl << "===================" << endl;
+}
+
 int main() {
     srand(time(NULL));
     Person person;
@@ -43,5 +51,16 @@ int main() {
     setPeople(people);
     listPeople(people);
     findLuckiest(people);
+    cout << "Now you can swap `Luck` of 2 Person, please choose from the list below" << endl;
+    listPeople(people);
+    int index1, index2;
+    cout << "Person 1 serial number: ";
+    cin >> index1;
+    cout << "Person 2 serial number: ";
+    cin >> index2;
+    changeLuck(people,index1,index2);
+    listPeople(people);
+    Person *man = new Person();
+    delete man;
     return 0;
 }
